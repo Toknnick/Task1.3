5ee0a0651bd9276d2dee528945e1b7cb7cc4e0d4
Maksim <maksimus.tumannick@yandex.ru>
2022-06-20 19:27:32 +0300

    Task4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Где Вы живете?");
+            string home = Console.ReadLine();
+            Console.WriteLine("Где Вы работаете?");
+            string work = Console.ReadLine();
+
+            Console.WriteLine($"Вы {name}, вам {age} год, вы живете {home}, работаете на {work}");
+
+        }
+    }
+}
